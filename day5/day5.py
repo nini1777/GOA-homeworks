0e954ebaf87feaf17cdc7e54adeddd9dfd1f3b9f
nini1777 <surmava.nini@gmail.com>
2024-07-07 03:52:22 +0400

    nini1777

---
+++ day5/day5.py
@@ -0,0 +1,8 @@
+print (35 + 17)
+print (16 - 12)
+print (23 * 5)
+print (90 / 3) 
+print (44 // 5) #we learn this too, this helps us to calculate how many times 5 will fit in 44
+
+country = "georgia"
+print (country) #string 
