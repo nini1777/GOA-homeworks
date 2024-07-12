6d745198cd853e7db6922d99cfc0f3b593735797
nini1777 <surmava.nini@gmail.com>
2024-07-12 13:43:40 +0400

    nini1777

---
+++ day6/homework.2.py
@@ -0,0 +1,8 @@
+age = input()
+print ("welcome " + age )
+
+surname = input()
+print("welcome " + surname ) 
+
+height = input()
+print("welcome" + height )
---
+++ day6/homework.3.py
@@ -0,0 +1,3 @@
+user_name = input()
+last_name = input()
+print("hello " + user_name + last_name )
